a0146fdade2594c45c07d6d0f66c5c8589edda85
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-02-25 17:58:13 +0530

    style: timer clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Nav from "./components/core/Nav";
 import Sidebar from "./components/core/Sidebar";
+import TimerClock from "./components/core/TimerClock";
 import './css/style.css';
+
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
       <Sidebar></Sidebar>
+      <TimerClock hours="00" minutes="20" seconds="00"></TimerClock>
     </div>
   );
 }
