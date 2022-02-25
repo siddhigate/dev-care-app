4d3f87c6378c5919f263d8c0f518f26f20ee5730
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-02-25 18:18:47 +0530

    style: cartoon score

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Nav from "./components/core/Nav";
+import Score from "./components/core/Score";
 import Sidebar from "./components/core/Sidebar";
 import TimerClock from "./components/core/TimerClock";
 import Toggles from "./components/core/Toggles";
@@ -12,6 +13,7 @@ function App() {
       <Sidebar></Sidebar>
       <TimerClock hours="00" minutes="20" seconds="00"></TimerClock>
       <Toggles/>
+      <Score/>
     </div>
   );
 }
---
+++ src/components/core/Score.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Score = () => {
+    return (
+        <div className="cartoon flex flex-col flex-justify-center flex-items-center">
+            <img src="./assets/cartoon/smileydev.png" alt="" />
+            <p className="txt-center fs-lg">You are taking good care of your health!</p>
+        </div>
+    );
+};
+
+export default Score;
