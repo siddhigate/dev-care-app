43112cf84f5f8817924e84de1a8545c8e64708fb
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-02-25 17:48:19 +0530

    style: sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./components/core/Nav";
+import Sidebar from "./components/core/Sidebar";
 import './css/style.css';
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <Sidebar></Sidebar>
     </div>
   );
 }
---
+++ src/components/core/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+      <ul className="sidebar-links">
+        <li>
+          <i className="fa-solid fa-house-chimney"></i>
+          <span className="fw-bold">Dashboard</span>
+        </li>
+        <li>
+          <i className="fa-solid fa-child"></i>
+          <span>Back care</span>
+        </li>
+        <li>
+          <i className="fa-solid fa-eye"></i>
+          <span>Eye Care</span>
+        </li>
+        <li>
+          <i className="fa-solid fa-ear-listen"></i>
+          <span>Ear Care</span>
+        </li>
+        <li>
+          <i className="fa-solid fa-gear"></i>
+          <span>Settings</span>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
